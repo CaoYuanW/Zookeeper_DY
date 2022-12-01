54c41f695eaa575629b43450eab3fa4126827099
CaoYuan <1667854547@qq.com>
2022-12-01 20:55:58 +0800

    One

--- src/main/java/day01/Demo01.java
+++ src/main/java/day01/Demo01.java
@@ -1,7 +1,20 @@
 package day01;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
+
 public class Demo01 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+
+        // 创建Zookeeper对象
+        ZooKeeper zk = new ZooKeeper("linux01:2181", 2000, null);
+
+        //创建节点
+        zk.create("/aaa","aaa".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+
+        // 关闭连接
+        zk.close();
     }
 }
